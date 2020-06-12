f4d30b658e32fe05bb3220f3a570369487c1e179
Megan Hepner <megan.hepner@gmail.com>
2020-06-12 08:43:39 -0700

    create initial js functions with business and user logic separate

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-$(document).ready(function() {
+//Business Logic
+function MrRobo(number){
+  let numbers = [];
   
-});
+ console.log(number);
+}
+
+
+//User Logic
+$(document).ready(function() {
+  $("form#userNumber").submit(function(event) {
+    event.preventDefault();
+    const number = parseInt($("input#number").val());
+    const reply = MrRobo(number);
+    $("#result").text(reply);
+    });
+  });
