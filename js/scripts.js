7e76df5c2e4cfcd74e324c3e0f4bd7ece975369b
Megan Hepner <megan.hepner@gmail.com>
2020-06-12 15:29:52 -0700

    begin to combine seperate functioning loops

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,38 @@
 //Business Logic
 function MrRobo(number){
-
-  let array = [0];
-  count = 0;
   if (number.includes("3")){
     return "Won't you be my neighbor";
     } else if (number.includes("2")){
       return "Boop!"
     } else if (number.includes(1)){
       return "Beep!"
-    }
-// //   } else {
-// //     while (count < number){
-// //     count = count+1;
-// //     array.push(count);
-// //     console.log(array);
-// //   };
-};
+    } else {
+      const number = parseInt($("input#number").val());
+      let numbers = [0];
+      count = 0;
+        while (count < number){
+        count = count+1;
+        numbers.push(count);
+        numbers[1] = " Beep!";
+        console.log(numbers);
+       }
+        // let index = numbers.indexOf("1");
+        // numbers[index] = " Beep!";
+        // console.log(numbers);
+     };
+    };
 
-
-
-//   for (let i = 0; i <= number; i++){
-
-//     array.push(+1);
-//   }
-// }
-
-  
-
-//   for (let i =0; i < numbers.length; i++){
-//     let index = numbers.indexOf("1");
-//     numbers[index] = " Beep!";
-//  };
-  
-//   for (let i =0; i < numbers.length; i++){
-//     let index = numbers.indexOf("2");
-//     numbers[index] = " Boop!";
-//   };
-
-//   for (let i =0; i < numbers.length; i++){
-//     let index = numbers.indexOf("3");
-//     numbers[index] = " Won't you be my neighbor?";
-//   };
-
-// //   return numbers;
-// }
-  
-
+     
+      
+      // for (let i =0; i < numbers.length; i++){
+      //   let index = numbers.indexOf("2");
+      //   numbers[index] = " Boop!";
+      // };
+    
+      // for (let i =0; i < numbers.length; i++){
+      //   let index = numbers.indexOf("3");
+      //   numbers[index] = " Won't you be my neighbor?";
+      // };
 
 
 
